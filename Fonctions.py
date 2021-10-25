a4e9ce50309087763eaa5dd2494c617965e1d9c1
Gwendal <gwetre@gmail.com>
2021-10-25 16:26:04 +0200

    Création des fonctions des B1 et 2 sans tests

--- Fonctions.py
+++ Fonctions.py
@@ -44,33 +44,40 @@ def gagnant(scores):
 
 # B1 - Tour d'un joueur
 def continuer():
-    pass
-    # Fonction à completer
+    return input_protege("Souhaitez-vous continuer?", str, "list", (), ["oui", "Oui", "OUI", "non", "Non", "NON"]) in ["oui", "Oui", "OUI"]
 
 
-def tour_joueur(j):
-    pass
-    # Fonction à completer
+def tour_joueur(j, joueurs, pioche, scores):
+    veut_continuer = continuer()
+    if veut_continuer:
+        scores[j] += valeur_carte(pioche[0])
+    if not veut_continuer or scores[j] > 21:
+        joueurs.remove(j)
+
+    # Fonction à tester
 
 
 # B2 - Une partie complète
-def tour_complet():
-    pass
-    # Fonction à completer
+def tour_complet(joueurs, pioche, scores):
+    for j in joueurs:
+        tour_joueur(j, joueurs, pioche, scores)
+    # Fonction à tester
 
 
-def partie_finie():
-    pass
-    # Fonction à completer
+def partie_finie(joueurs, scores):
+    return (21 in scores.values()) or (joueurs == [])
+    # Fonction à tester
 
 
-def partie_complete():
-    pass
-    # Fonction à completer
+def partie_complete(joueurs, pioche, scores, victoires):
+    while not partie_finie(joueurs, scores):
+        tour_complet(joueurs, pioche, scores)
+    victoires[gagnant(scores)] += 1
+    # Fonction à tester
 
 
 # E - Diverses fonctions supplémentaires
-def protected_input(question, input_type, range_or_list, range_to_fit_in, list_to_fit_in):
+def input_protege(question, type_attendu, type_ensemble, intervalle_reponses_possibles, liste_reponses_possibles):
     """
     question=question to ask the user
     input_type=type of the input needed (example: int, float, str)
@@ -78,34 +85,36 @@ def protected_input(question, input_type, range_or_list, range_to_fit_in, list_t
     range= range the input needs to be part of (example: (4, 8) here 8 is not included while 4 is) leave as empty if not applicable
     list= list the input needs to be part of (example: [4, 8] or list1 ) leave as empty if not applicable
     """
-    raw_input = input(question)
-    type_was_checked = False
-    value_was_checked = False
+    saisie = input(question)
+    type_verifie = False
+    valeur_verifie = False
 
-    while not(type_was_checked and value_was_checked):
+    while not(type_verifie and valeur_verifie):
         try:
-            partially_processed_input = input_type(raw_input)
+            saisie_modifie = type_attendu(saisie)
 
         except:
-            print("Your input was not a ", input_type.__name__, ". Please enter a ", input_type.__name__)
-            raw_input = input()
+            print("Votre saisie n'est pas du type ", type_attendu.__name__, ". Merci de saisir un ", type_attendu.__name__)
+            saisie = input()
 
         else:
-            type_was_checked = True
-            if range_or_list == "range":
-                if partially_processed_input in range(range_to_fit_in[0], range_to_fit_in[1]):
-                    value_was_checked = True
+            type_verifie = True
+            if type_ensemble == "range":
+                if saisie_modifie in range(intervalle_reponses_possibles[0], intervalle_reponses_possibles[1]):
+                    valeur_verifie = True
                 else:
-                    print("Your input was not in range ", range_to_fit_in, ". Please enter a number in range ", range_to_fit_in)
-                    print(value_was_checked)
-                    raw_input = input()
-            elif range_or_list == "list":
-                if partially_processed_input in list_to_fit_in:
-                    value_was_checked = True
+                    print("Votre saisie n'est pas comprise dans l'intervalle : ", intervalle_reponses_possibles, ". Merci de saisir une valeur comprise dans : ", intervalle_reponses_possibles)
+                    print(valeur_verifie)
+                    saisie = input()
+            elif type_ensemble == "list":
+                if saisie_modifie in liste_reponses_possibles:
+                    valeur_verifie = True
                 else:
-                    print("Your input was not in list ", list_to_fit_in, ". Please enter a number in list ", list_to_fit_in)
-                    raw_input = input()
+                    print("Votre saisie n'est pas comprise dans la liste : ", liste_reponses_possibles, ". Merci de saisir une valeur comprise dans : ", liste_reponses_possibles)
+                    saisie = input()
             else:
-                value_was_checked = True
-            print(value_was_checked)
-    return partially_processed_input
+                valeur_verifie = True
+            print(valeur_verifie)
+    return saisie_modifie
+
+
