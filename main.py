07a327a19fd8c4489a933b13d9d3d4ca44edc97a
gwt734 <51045195+gwt734@users.noreply.github.com>
2021-10-29 16:19:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,19 @@ import Fonctions
 
 
 def main():
-    nb_joueurs = Fonctions.input_protege("Combien de joueurs jouerons?", int, "range", (1, 15))
-    joueurs = Fonctions.init_joueurs(nb_joueurs)
-    veut_continuer = True
+    nb_joueurs = Fonctions.input_protege("Combien de joueurs jouerons?", type_attendu=int, type_ensemble="range", intervalle_reponses_possibles=(1, 15)) # Demande le nombre de joueurs
+    joueurs = Fonctions.init_joueurs(nb_joueurs)    # Créé la liste des joueurs
+    veut_rejouer = True
     victoires = Fonctions.init_scores(joueurs)
-    while veut_continuer:
+    while veut_rejouer:
+        joueurs_partie = joueurs
         pioche = Fonctions.init_pioche(nb_joueurs)
         print(pioche)
-        scores = Fonctions.premier_tour(joueurs, pioche)
+        scores = Fonctions.premier_tour(joueurs_partie, pioche)
         print(pioche)
-        Fonctions.partie_complete(joueurs, pioche, scores, victoires)
+        Fonctions.partie_complete(joueurs_partie, pioche, scores, victoires)
         print(victoires)
-        veut_continuer = Fonctions.continuer()
+        veut_rejouer = Fonctions.continuer()
 
     # Fonction à tester
 
